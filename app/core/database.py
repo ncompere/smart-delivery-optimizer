1e553cfaa137221215adcaf2fa6ac39dc4d0b44b
Nicolas Compère <75181320+ncompere@users.noreply.github.com>
2026-02-16 22:39:12 +0100

    Ajout du fichier Dockerfile, mise à jour de la configuration de la base de données pour utiliser les variables d'environnement, et mise à jour des dépendances dans pyproject.toml et poetry.lock.

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/delivery_optimizer"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/delivery_optimizer"
+)
 
 engine = create_engine(DATABASE_URL)
 
